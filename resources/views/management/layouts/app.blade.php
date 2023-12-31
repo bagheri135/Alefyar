97669cdb486c96cf7de1509acea46b26666f1e55
Mehdi Bagheri <pr.mehdi.bagheri@gmail.com>
2023-12-31 20:22:35 +0330

    Start Alefyar Project

--- resources/views/management/layouts/app.blade.php
+++ resources/views/management/layouts/app.blade.php
@@ -4,27 +4,30 @@
 @include('management.layouts.head')
 
 <body>
-<div class="container-scroller">
-    <!-- partial:partials/_navbar.html -->
-@include('management.layouts.navbar')
-<!-- partial -->
-    <div class="container-fluid page-body-wrapper">
-        <!-- partial:partials/_sidebar.html -->
-    @include('management.layouts.sidebar')
-    <!-- partial -->
-        <div class="main-panel">
-        @yield('content')
-        <!-- content-wrapper ends -->
-            <!-- partial:partials/_footer.html -->
-        @include('management.layouts.footer')
+    <div class="container-scroller">
+        <!-- partial:partials/_navbar.html -->
+        @include('management.layouts.navbar')
         <!-- partial -->
+        <div class="container-fluid page-body-wrapper">
+            <!-- partial:partials/_sidebar.html -->
+            @include('management.layouts.sidebar')
+            <!-- partial -->
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    @include('management.layouts.header')
+                    @yield('content')
+                </div>
+                <!-- content-wrapper ends -->
+                <!-- partial:partials/_footer.html -->
+                @include('management.layouts.footer')
+                <!-- partial -->
+            </div>
+            <!-- main-panel ends -->
         </div>
-        <!-- main-panel ends -->
+        <!-- page-body-wrapper ends -->
     </div>
-    <!-- page-body-wrapper ends -->
-</div>
-@include('management.layouts.scripts')
-@yield('script')
+    @include('management.layouts.scripts')
+    @yield('script')
 </body>
 
 </html>
---
+++ resources/views/management/layouts/header.blade.php
@@ -0,0 +1,10 @@
+        <!-- Page Title Header Starts-->
+        <div class="row page-title-header">
+            <div class="col-12">
+                <div class="page-header">
+                    <h4 class="page-title">@yield('header-title')</h4>
+                </div>
+            </div>
+
+        </div>
+        <!-- Page Title Header Ends-->
